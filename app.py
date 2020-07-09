5e1c0c3ffb131f3ea965b10326c7e3a2a8d3eba5
clavesi <34937562+RealRavenn@users.noreply.github.com>
2020-07-09 13:00:12 -0500

    Adjust sigmoid functions for circle radius and mortality color

--- app.py
+++ app.py
@@ -29,6 +29,15 @@ def coronavirusMap():
 
     countries = []
 
+    confirmedCasesList = []
+    ratesList = []
+    for case in cases:
+        confirmedCasesList.append(cases[case][-1]['confirmed'])
+        ratesList.append(cases[case][-1]['deaths']/cases[case][-1]['confirmed'])
+
+    print(max(confirmedCasesList))
+    print(max(ratesList))
+
     for case in cases:
         confirmed = cases[case][-1]['confirmed']
         deaths = cases[case][-1]['deaths']
@@ -40,15 +49,15 @@ def coronavirusMap():
                 lon = latlon[latlon.index(case) + 2]
 
                 smallSize = 10 # Plateau small countries
-                bigSize = -1/10000 # Plateau big countries
+                bigSize = -1 / (max(confirmedCasesList) * .01) # Plateau big countries
                 radius = confirmed
                 # Sigmoid function to get proper circle radius
                 radius = (1/(1 + smallSize * math.exp(bigSize * radius))) * 1000000
 
-                lowMortality = 1/50 # Plateau low mortality rates
+                lowMortality = 1/100 # Plateau low mortality rates
                 color = int((deaths / confirmed) * 100)
                 # Sigmoid function to get a red RGB value
-                sigmoid = int((1/(1 + lowMortality * math.exp(-1/10 * color + 5))) * 255)
+                sigmoid = int((1/(1 + lowMortality * math.exp(-1/7 * color + 6))) * 255)
                 # Convert red RGB to hex
                 color = '#{:02x}{:02x}{:02x}'.format(sigmoid, 0, 0)
 
@@ -62,7 +71,7 @@ def coronavirusMap():
             color=country[6],
             fill=True,
             fill_color=country[6],
-            fill_opacity=.5
+            fill_opacity=.3
         ).add_to(m)
 
     m.save(outfile='templates/map.html')
